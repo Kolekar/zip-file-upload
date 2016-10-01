85ed71b4f791099991935e079a9e0b30670e1d1a
Harikesh Kolekar <harikeshkolekar@gmail.com>
2016-10-01 19:59:38 +0530

    add highlight to xml show

--- app/controllers/upload_files_controller.rb
+++ app/controllers/upload_files_controller.rb
@@ -94,9 +94,12 @@ class UploadFilesController < ApplicationController
   end
 
   def parse_xml_string(entry)
-    doc = Nokogiri::XML.parse(entry.get_input_stream.read)
-    results = doc.xpath(params[:filter])
+    @random_string = [*('A'..'Z'),*('a'..'z')].sample(20).join
+    @doc = Nokogiri::XML.parse(entry.get_input_stream.read)
+    results = @doc.xpath(params[:filter])
     results.map do |x|
+      node = Nokogiri::XML::Node.new @random_string, @doc
+      x.add_previous_sibling(node) if @doc.root != x
       @content << x.to_s
     end
   rescue
